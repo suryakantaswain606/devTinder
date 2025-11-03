3462fb59d41bdd105d0aac1bc2e49a4fbb7fd771
Suryakanta Swain <suryakantaswain606@gmail.com>
2025-11-03 23:41:13 +0530

    Schema validations

--- src/app.js
+++ src/app.js
@@ -12,13 +12,84 @@ const app = express();
 //Middleware to parse JSON to object and store to req.body
 app.use(express.json());
 
+app.get("/user", async (req, res) => {
+  try {
+    const user = await User.findOne({ emailId: req.body.email });
+    if (user.length === 0) {
+      return res.status(404).send("user not found");
+    }
+    res.send(user);
+  } catch (err) {
+    res.status(500).send("Error fetching user");
+  }
+});
+
+app.delete("/user", async (req, res) => {
+  const id = req.body.id;
+  try {
+    const deletedUser = await User.findByIdAndDelete(id);
+    if (deletedUser == null) {
+      return res.status(404).send("User not found or already deleted");
+    }
+    res.send("user deleted successfully");
+  } catch (err) {
+    res.status(400).send("something went wrong");
+  }
+});
+
+app.get("/feed", async (req, res) => {
+  try {
+    const allUsers = await User.find({});
+    if (allUsers.length === 0) {
+      return res.status(404).send("No user there");
+    }
+    res.send(allUsers);
+  } catch (err) {
+    res.status(500).send("Error Fetching Users");
+  }
+});
+
+app.patch("/user", async (req, res) => {
+  try {
+    // const userId = req.body.userId;
+    const emailId = req.body.emailId;
+    const age = req.body.age;
+    const data = req.body;
+
+    // const user = await User.findByIdAndUpdate(userId, data, {
+    //   new: "true",
+    // });
+
+    // const user = await User.findOneAndUpdate({ _id: userId }, data, {
+    //   new: true,
+    // });
+
+    const user = await User.findOneAndUpdate({ emailId: emailId }, data, {
+      new: true,
+    });
+
+    // const user = await User.updateMany({ age: age }, data, {
+    //   runValidators: true,
+    // });
+    // const updatedUsers = await User.find({ age: age });
+    if (!user) {
+      return res.status(404).send("User not found");
+    }
+
+    res.status(200).send(user);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error updating user");
+  }
+});
+
 app.post("/userPost", async (req, res) => {
   try {
     const user = new User(req.body);
     await user.save();
     res.send("userPost added successfully");
   } catch (err) {
-    res.status(500).send("posting not successful");
+    res.status(500).send(err.message);
   }
 });
 
--- src/models/user.js
+++ src/models/user.js
@@ -1,9 +1,31 @@
 const mongoose = require("mongoose");
-const userSchema = new mongoose.Schema({
-  firstName: { type: String },
-  lastName: { type: String },
-  age: { type: Number },
-});
+const userSchema = new mongoose.Schema(
+  {
+    firstName: { type: String, required: true },
+    lastName: { type: String },
+    emailId: {
+      type: String,
+      unique: true,
+      required: true,
+      lowercase: true,
+      maxLength: 20,
+      trim: true,
+      match: /^[^\s@]+@[^\s@]+\.[^\s@]+$/, // regex for valid emails
+    },
+    age: { type: Number, min: 1 },
+    gender: {
+      type: String,
+      validate(value) {
+        if (!["male", "female", "other"].includes(value)) {
+          throw new Error("add gender from earth");
+        }
+      },
+    },
+    objective: { type: String, default: "nodejs learning" },
+    skills: { type: [String] },
+  },
+  { timestamps: true }
+);
 
 const User = mongoose.model("User", userSchema);
 
