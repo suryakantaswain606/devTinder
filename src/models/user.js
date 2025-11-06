9ab6b0c4620b74438a847611d0642edd8953fec3
Suryakanta Swain <suryakantaswain606@gmail.com>
2025-11-06 15:39:30 +0530

    added API and DB level validation and used validator library.

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 const userSchema = new mongoose.Schema(
   {
     firstName: { type: String, required: true },
@@ -10,19 +11,34 @@ const userSchema = new mongoose.Schema(
       lowercase: true,
       maxLength: 20,
       trim: true,
-      match: /^[^\s@]+@[^\s@]+\.[^\s@]+$/, // regex for valid emails
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("invalid email address : " + value);
+        }
+      },
     },
     age: { type: Number, min: 1 },
     gender: {
       type: String,
       validate(value) {
         if (!["male", "female", "other"].includes(value)) {
-          throw new Error("add gender from earth");
+          throw new Error("add either Male or Female or Other");
         }
       },
     },
     objective: { type: String, default: "nodejs learning" },
     skills: { type: [String] },
+    photoURL: {
+      type: String,
+      validate(value) {
+        if (!validator.isURL(value)) {
+          throw new Error("Invalid photo URL : " + value);
+        }
+      },
+      default:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_g_7YVzERozXI_mfnbSPkggiXqlljwtCQXw&s",
+    },
+    password: { type: String, select: false, required: true },
   },
   { timestamps: true }
 );
